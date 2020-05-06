1d039976badc4d862ffd207ac855c89c9071acab
Crystal Ly <crystal.m.ly@gmail.com>
2020-05-06 16:27:52 -0500

    removed node_modules/reinstalled node_modules

--- routes/api/index.js
+++ routes/api/index.js
@@ -10,8 +10,8 @@ router.use("/info", InfoRoutes);
 router.use("/manuals", ManualRoutes);
 
 // For anything else, render the html page
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../../client/public/index.html"));
 });
 
 module.exports = router;
