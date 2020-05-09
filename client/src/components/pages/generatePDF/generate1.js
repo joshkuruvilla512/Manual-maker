c00f69ce81034a38d715d995dd535afc1450534e
briannamccray <bbotting1@gmail.com>
2020-05-08 19:34:17 -0500

    Consoled the template literal to show what we need from DB

---
+++ client/src/components/pages/generatePDF/generate1.js
@@ -0,0 +1,22 @@
+
+function generateHTML(data) {
+    console.log("Generate PDF");
+    console.log(data);
+    return `<!DOCTYPE html>
+  <html lang="en">
+     <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+        <title>GENERATE 1</title>
+        
+     </head>
+     <body>
+        <h1> Name: ${data} </h1>
+     </body>
+  </html>`;
+}
+
+module.exports = generateHTML;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,12 +3,12 @@ import axios from "axios";
 //grabbing info front front end and making a call to backend to create
 export default {
   addInfo: function (infoData) {
-    console.log(infoData);
+    console.log("ADDINFO");
     return axios.post("/api/info", infoData);
   },
 
   getInfo: function () {
-    console.log("TRYIN");
+    console.log("GETINFO");
     return axios.get("/api/info");
   }
 
