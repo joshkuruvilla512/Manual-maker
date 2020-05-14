9de1a832deb1ce86cbf96709380fd9176460f7ce
Crystal Ly <crystal.m.ly@gmail.com>
2020-05-13 21:37:20 -0500

    removed chakra-ui and added reactstrap

--- client/src/components/utilityComponents/MenuNavigation.js
+++ client/src/components/utilityComponents/MenuNavigation.js
@@ -1,15 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 
 
-class MenuNavigation extends Component {
-    render() {
-        return (
-            <div>
-                Menu Navigation Buttons here
-            </div>
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+} from 'reactstrap';
 
-        );
-    }
+const MenuNavigation = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">Manual Maker</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/components/">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/reactstrap/reactstrap">Create</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Options
+                </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    About Us
+                  </DropdownItem>
+                                <DropdownItem>
+                                    Contact Us
+                  </DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem>
+                                    How it Works
+                  </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
 }
 
 export default MenuNavigation;
